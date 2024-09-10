b9de1d1752537dc130a717fbed26d16ce9afcb05
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-09-11 00:27:21 +0300

    modifications for the register functionality

--- Public/src/pages/Register.jsx
+++ Public/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../Assets/logo.svg";
 import {ToastContainer, toast} from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
@@ -8,6 +8,7 @@ import axios from "axios";
 import { registerRoute } from "../utils/APIRoutes";
 
 function Register() {
+    const navigate = useNavigate();
     const [values, setValues] = useState({
         username: "",
         email: "",
@@ -27,10 +28,19 @@ function Register() {
         event.preventDefault();
         if(handleValidation()) {
             console.log("in validation ", registerRoute)
-            const {username, email, password, confirmPassword} = values;
+            const {username, email, password} = values;
             const {data} = await axios.post(registerRoute, {
                 username, email, password,
             })
+            if (data.status === false){
+                toast.error(data.msg, toastOptions);
+            }
+
+            if (data.status === true){
+                localStorage.setItem("chat-app-user", JSON.stringify(data.user));
+            }
+
+            navigate("/");
         }
     }
 
