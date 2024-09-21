eaa0b57cfc122b44c40b60cf8ea2174d16e25a88
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-09-21 22:01:10 +0300

    adding styles to set avatar page

--- Public/src/pages/SetAvatar.jsx
+++ Public/src/pages/SetAvatar.jsx
@@ -43,23 +43,59 @@ function SetAvatar() {
 
     return (
         <>
-            <container>
-                <div className="title-container">Pick an avatar as your profile picture</div>
+            <Container>
+                <div className="title-container"><h1>Pick an avatar as your profile picture</h1></div>
                 <div className="avatars">
                     {avatars.map((avatar, index) => {
                         return (
                             <div key={index} className={`avatar ${selectedAvatar === index ? "selected" : ""}`}>
-                                <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar" onClick={()=> selectedAvatar(index)}/>
+                                <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar" onClick={()=> setSelectedAvatar(index)}/>
                             </div>
                         )
                     })}
                 </div>
-            </container>
+            </Container>
             <ToastContainer/>
         </>
     )
 }
 
-const container = styled.div``;
+const Container = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 3rem;
+    background-color: #131324;
+    height: 100vh;
+    width: 100vw;
+    .loader {
+        max-inline-size: 100%
+    }
+    .title-container {
+        h1 {
+            color: white;
+        }
+    }
+    .avatars {
+        display: flex;
+        gap: 2rem;
+        .avatar {
+            border: 0.4rem solid transparent;
+            padding: 0.4rem;
+            border-radius: 5rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: 0.5s east-in-out;
+            img {
+                height: 6rem;
+            }
+        }
+        .selected {
+            border: 0.4rem solid #4e0eff;
+        }
+    }
+`;
 
 export default SetAvatar;
